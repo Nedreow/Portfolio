786a6feb2a4339be801bc1ae898852efd92081cc
Nedreow <F.vanwoerden23@gmail.com>
2015-11-09 16:12:33 +0100

    Added Integer to Binary function to JS Showcase

--- function showcase.js
+++ function showcase.js
@@ -7,5 +7,46 @@ function removeValueFromArray (arrayToEdit, removeValue)
   while (arrayToEdit.indexOf(removeValue) != absent)
   {
     arr.splice(arrayToEdit.indexOf(removeValue),1);
-  }    
+  }
+  
+  return arrayToEdit;
+}
+
+//Return binary 
+function integerToBinary(integerToTranslate)
+{
+  //find highest multiple of two that is less than the integer
+  powerOf2 = lowerPowerOf2(integerToTranslate);
+  
+  binaryNumber = '';
+  
+  while (integerToTranslate != 0)
+  {
+    if (powerOf2 <= integerToTranslate)
+    {
+      integerToTranslate = integerToTranslate - powerOf2;
+      
+      binaryNumber.concat('1');
+    }
+    else
+    {
+      binaryNumber.concat('0');
+    }
+    powerOf2 = powerOf2/2;
+  }
+  
+  return binaryNumber;
+}
+
+function lowerPowerOf2(limitInteger)
+{
+  //find highest multiple of two that is less than the integer
+  var multipleOf2 = 1;
+  var halfInteger = limitInteger / 2;
+  
+  while (multipleOf2 < halfInteger)
+  {
+    multipleOf2 = multipleOf2 + multipleOf2;
+  }
+  return multipleOf2;
 }
